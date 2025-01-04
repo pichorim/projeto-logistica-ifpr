7252a218c5e360902a46d509351814ee3978f5c9
odairmonteschio <odairmonteschio@gmail.com>
2025-01-04 20:25:18 -0300

    Update Conexao.java

--- JDBC/src/Conexao.java
+++ JDBC/src/Conexao.java
@@ -4,55 +4,58 @@ public class Conexao{
 
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int op=1,nDeColun;
-        String tabela="pessoa",variaveis[],query;
+        int op = 1, nDeColun;
+        String tabela = "pessoa", query;
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/correios","odair","131619");
-            Statement stmt=con.createStatement();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/correios", "odair", "131619");
+            Statement stmt = con.createStatement();
             ResultSet rs;
             ResultSetMetaData rsmd;
             System.out.println("\nConexão estabelecida");
 
-            while(op!=0){
+            while(op != 0){
                 System.out.println("\nDigite:\n1: Select\n2: Insert\n3: Update\n4: Delete\n");
-                op=sc.nextInt();sc.nextLine();
+                op = sc.nextInt(); sc.nextLine();
+
                 switch(op){
                     case 1: //Select
-                        rs=stmt.executeQuery("select * from "+tabela+";");
+                        rs = stmt.executeQuery("select * from " + tabela + ";");
                         rsmd = rs.getMetaData();
                         while(rs.next()){
-                            for(int i =1;i<=rsmd.getColumnCount();i++){
-                                if(rs.getString(i)!=null){
-                                    System.out.println(rs.getString(i)+" ");
+                            for(int i = 1; i <= rsmd.getColumnCount(); i++){
+                                if(rs.getString(i) != null){
+                                    System.out.println(rs.getString(i) + " ");
                                 }
                             }
                         }
                         break;
+
                     case 2: //Insert
                     //dar um jeito dele pergar o null
-                        query = "insert into "+tabela+" values (";
-                        rs=stmt.executeQuery("select * from "+tabela+";");
+                        query = "insert into " + tabela + " values (";
+                        rs = stmt.executeQuery("select * from " + tabela + ";");
                         rsmd = rs.getMetaData();
                         nDeColun = rsmd.getColumnCount();
-                        for(int i = 1;i<=nDeColun;i++){
-                            if(i<nDeColun){
-                                query = query + "'" +sc.nextLine()+ "',";
+                        for(int i = 1; i <= nDeColun; i++){
+                            if(i < nDeColun){
+                                query = query + "'" + sc.nextLine() + "',";
                             }
                             else{
-                                query = query + "'" +sc.nextLine()+ "');";
+                                query = query + "'" + sc.nextLine() + "');";
                             }
                         }
                         stmt.executeUpdate(query);
                         break;
+
                     case 3: //Update
-                        query = "update "+tabela+" set ";
-                        rs=stmt.executeQuery("select * from "+tabela+";");
+                        query = "update " + tabela + " set ";
+                        rs = stmt.executeQuery("select * from " + tabela + ";");
                         rsmd = rs.getMetaData();
                         nDeColun = rsmd.getColumnCount();
-                        for(int i = 2;i<=nDeColun;i++){
-                            if(i<nDeColun){
+                        for(int i = 2; i <= nDeColun; i++){
+                            if(i < nDeColun){
                                 query = query + rsmd.getColumnName(i) + " = '" + sc.nextLine() + "', ";
                             }
                             else{
@@ -61,8 +64,11 @@ public class Conexao{
                         }
                         stmt.executeUpdate(query + "where " + rsmd.getColumnName(1) + " = " + sc.nextLine() + ";");
                         break;
+                        
                     case 4: //Delete
-                        stmt.executeUpdate("delete from "+tabela+" where cpf = '"+sc.nextLine()+"'");
+                        stmt.executeUpdate("delete from " + tabela + " where cpf = '" + sc.nextLine() + "'");
+                        break;
+                    default:
                         break;
                 }
             }
