60106375218190299566d573af4e55c801d0dd55
odairmonteschio <odairmonteschio@gmail.com>
2025-01-06 13:05:07 -0300

    Delete Código/logica directory
    Substituição dos arquivos da v0.1.0 pra v1.0.0

--- Código/logica/Destinatario.java
+++
@@ -1,8 +0,0 @@
-package logica;
-
-public class Destinatario extends Pessoa {
-	
-	public Destinatario(String CPF,String primeiroNome,String sobrenome, String telefone, String email) {
-		super(CPF,primeiroNome,sobrenome,telefone,email);
-	}
-}
--- Código/logica/Remetente.java
+++
@@ -1,8 +0,0 @@
-package logica;
-
-public class Remetente extends Pessoa {
-	
-	public Remetente(String CPF,String primeiroNome,String sobrenome, String telefone, String email) {
-		super(CPF,primeiroNome,sobrenome,telefone,email);
-	}
-}
